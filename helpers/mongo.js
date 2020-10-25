3cad52effed7d1fa497b3cd52adc6d3dcd0f3ce6
jaideepghosh <er.jaideepghosh@gmail.com>
2020-10-25 16:33:35 +0530

    fix(helper):Updated mongo helper.

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,15 +1,18 @@
 import mongoose from "mongoose";
 export default async (connectionString) => {
-  try {
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
+    return new Promise((resolve, reject) => {
+        try {
+            await mongoose.connect(connectionString, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+            });
+            console.log("Connected to mongodb.");
+            return resolve(mongoose);
+        } catch (error) {
+            console.error("Mongoose connection error.");
+            reject(new Error("Unable to connect to mongodb."));
+        }
     });
-    console.log("Connected to mongodb.");
-  } catch (error) {
-    console.error("Mongoose connection error.");
-    throw new Error("Unable to connect to mongodb.");
-  }
 };

9a349fe832b15d95b6cf61510a6b4bf0a0b26c72
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-04 22:53:58 +0530

    feat(trends):Added timestamp in model.

--- models/trends.js
+++ models/trends.js
@@ -1,67 +1,70 @@
 import mongoose, { Schema } from "mongoose";
 
-const TrendSchema = new Schema({
-  video_id: {
-    type: String,
-    required: true
-  },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: false
-  },
-  video_url: {
-    type: String,
-    required: true
-  },
-  thumbnails: {
-    default: {
-      url: String,
-      width: Number,
-      height: Number
+const TrendSchema = new Schema(
+  {
+    video_id: {
+      type: String,
+      required: true
     },
-    medium: {
-      url: String,
-      width: Number,
-      height: Number
+    title: {
+      type: String,
+      required: true
     },
-    high: {
-      url: String,
-      width: Number,
-      height: Number
+    description: {
+      type: String,
+      required: false
     },
-    standard: {
-      url: String,
-      width: Number,
-      height: Number
+    video_url: {
+      type: String,
+      required: true
     },
-    maxres: {
-      url: String,
-      width: Number,
-      height: Number
+    thumbnails: {
+      default: {
+        url: String,
+        width: Number,
+        height: Number
+      },
+      medium: {
+        url: String,
+        width: Number,
+        height: Number
+      },
+      high: {
+        url: String,
+        width: Number,
+        height: Number
+      },
+      standard: {
+        url: String,
+        width: Number,
+        height: Number
+      },
+      maxres: {
+        url: String,
+        width: Number,
+        height: Number
+      }
+    },
+    channel: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "channels"
+    },
+    view_count: {
+      type: Number,
+      required: true
+    },
+    like_count: {
+      type: Number,
+      required: true
+    },
+    dislike_count: {
+      type: Number,
+      required: true
     }
   },
-  channel: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "channels"
-  },
-  view_count: {
-    type: Number,
-    required: true
-  },
-  like_count: {
-    type: Number,
-    required: true
-  },
-  dislike_count: {
-    type: Number,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 let Trend;
 try {

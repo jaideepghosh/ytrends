455780ca9c98e9c271f8ba0fa62014a582b9cda5
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-04 15:00:32 +0530

    feat(mongo):Updated structure.

--- helpers/mongo.js
+++ helpers/mongo.js
@@ -1,18 +1,29 @@
 import mongoose from "mongoose";
-export default async (connectionString) => {
-    return new Promise((resolve, reject) => {
-        try {
-            await mongoose.connect(connectionString, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
-            });
-            console.log("Connected to mongodb.");
-            return resolve(mongoose);
-        } catch (error) {
-            console.error("Mongoose connection error.");
-            reject(new Error("Unable to connect to mongodb."));
-        }
-    });
+
+export const getConnectionString = (
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_DATABASE
+) => {
+  return `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+};
+
+export default (connectionString) => {
+  console.log("connectionString::", connectionString);
+  return new Promise(async (resolve, reject) => {
+    try {
+      await mongoose.connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true
+      });
+      console.log("Connected to mongodb.");
+      return resolve(mongoose);
+    } catch (error) {
+      console.error("Mongoose connection error.");
+      reject(new Error("Unable to connect to mongodb."));
+    }
+  });
 };

fffadcbcb1e163fe1bdfe5f9fe18d8c77438a5c9
jaideepghosh <er.jaideepghosh@gmail.com>
2020-11-05 04:27:41 +0530

    feat:Added netlify config.

---
+++ next.config.js
@@ -0,0 +1,20 @@
+// const isProd = process.env.NODE_ENV === 'production';
+
+// const withPWA = require('next-pwa');
+
+// module.exports = withPWA({
+//   pwa: {
+//     disable: !isProd,
+//     dest: 'public',
+//   },
+// });
+
+const { withPlugins } = require("next-compose-plugins");
+const withOptimizedImages = require("next-optimized-images");
+
+// next.js configuration
+const nextConfig = {
+  target: "serverless"
+};
+
+module.exports = withPlugins([withOptimizedImages], nextConfig);
